15031194785c50c68f1305be746a372622daf542
2020064 <2020064@B001-10>
2023-09-12 11:47:00 +0200

    Add project files.

---
+++ teszt/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace teszt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("helló");
+            Console.ReadKey();
+        }
+    }
+}
